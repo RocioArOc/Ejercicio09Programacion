209c564d36e964f3901265af36361ead29018cf6
Rocio Arias Ocón <rao24gr034@student.esgerencia.com>
2025-02-12 19:17:18 +0100

    a

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var ditto=["¡Oh! ¿Querías que me transformara en un Charizard? Ups… salió un Magikarp.", "A veces olvido en qué me transformé y me asusto con mi propio reflejo.","Esto es muy divertiditto", "A veces me transformo en un Ditto. Solo para recordar cómo se siente."];
+var charmander=["¡Si quieres desafiarme, prepárate para sentir el calor de la batalla!","¿Órdenes? ¡Hago lo que quiero, cuando quiero!","¡No me subestimes solo porque soy un inicial evolucionado!","¡Vamos, dame un desafío real!","El fuego de mi alma nunca se apagará."];
+var sandslash=["¡No te preocupes, me haré bolita y rodaré hasta la victoria!","Crecí en el desierto, estoy acostumbrado a condiciones duras.", "Si alguien necesita protección, yo me encargo.","No hay obstáculo que mis garras no puedan superar."];
+var trainer1=["La clave para ganar no es solo la fuerza, sino la estrategia.","¡No te confíes demasiado! Aún tengo sorpresas bajo la manga."];
+var trainer2=["¡Esta es la prueba final! ¡Demuestra que tienes lo que se necesita!","¡No importa cuán difícil sea el desafío, siempre seguiré adelante!"];
+var bulbasur=["¡No me subestimes solo porque soy un inicial!", "¡Usaré Drenadoras para recuperar algo de energía!","¿Nos queda alguna manzana?","No quiero terminar como ensalada chamuscada...","¡Confía en mí y en mis lianas", "Me voy a dar un buen descanso bajo el sol"];
+
+var frases=[ditto,charmander,sandslash,bulbasur,trainer1,trainer2]
